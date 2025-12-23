64ba36237d9ee9000c2cf77627f40ee7a0f2187d
ArpanParajuli <arpanparajuli394@gmail.com>
2025-12-23 08:29:29 +0545

    learning unit testing , just implementation done

---
+++ StudentManagement.Test/StudentCreateTest.cs
@@ -0,0 +1,25 @@
+﻿using StudentManagement.Api.Application.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Test
+{
+    public class StudentCreateTest
+    {
+
+        CreateStudentCommand CreateStudentCommand;
+        public StudentCreateTest() {
+            CreateStudentCommand = new CreateStudentCommand("Arpan Parajuli" , "arpanparajuli07@gmail.com" , 20);
+        }
+
+        [Fact]
+        public void CreateStudentTestMethod()
+        {
+         
+            
+        }
+    }
+}
